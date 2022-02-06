255287c9f567f21dbc1b97e92ab84f3e77ef3399
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-02-06 13:40:52 +0100

    [MIG] 12.0 donation_bank_statement

--- donation_bank_statement/__manifest__.py
+++ donation_bank_statement/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Donation Bank Statement',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Creates donation from unreconciled bank statement lines',
